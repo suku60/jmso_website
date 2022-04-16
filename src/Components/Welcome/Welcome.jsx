f20ebbf65e6f76629a3ebfeea19f484deb3d2619
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-04-16 14:55:01 +0200

    Added frontend.svg to welcome text. Rooted some variant colors based on cyberpuk palette.

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -1,7 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {ReactComponent as NameSvg} from '../../assets/svg/name.svg';
 import {ReactComponent as SecondNameSvg} from '../../assets/svg/secondname.svg';
+import {ReactComponent as SecondName2Svg} from '../../assets/svg/secondname2.svg';
 import {ReactComponent as SurnameSvg} from '../../assets/svg/surname.svg';
+import {ReactComponent as Surname2Svg} from '../../assets/svg/surname2.svg';
+import {ReactComponent as FrontendSvg} from '../../assets/svg/frontend.svg';
 import './Welcome.css';
 
 const Welcome = () => {
@@ -11,7 +14,7 @@ const Welcome = () => {
     let animationGlitch3 = "animationGlitchSquare3";
     let animationOff = "staticInvisible";
 
-    const [bgWelcomeBox, setBgWelcomeBox] = useState("hsl(0, 100%, 50%, .4)");
+    const [bgWelcomeBox, setBgWelcomeBox] = useState('var(--color-red-0-2)');
     
     const [screenAnimation, setScreenAnimation] = useState(animationOff)
     
@@ -41,8 +44,11 @@ return (
                 </div>
                 <NameSvg className='welcome_name'/>
                 <SecondNameSvg className='welcome_secondname'/>
+                <SecondName2Svg className='welcome_secondname2'/>
                 <SurnameSvg className='welcome_surname'/>
-                {/* <div className="welcome_photo"><img className='photo_avatar' id={screenInsideAnimation3} src='https://avatars.githubusercontent.com/u/96541489?v=4'/></div> */}
+                <Surname2Svg className='welcome_surname2'/>
+                <FrontendSvg className='welcome_frontend'/>
+                <div className="welcome_photo"><img className='photo_avatar' id={screenInsideAnimation3} src='https://avatars.githubusercontent.com/u/96541489?v=4'/></div>
             </div>
        )
 }
